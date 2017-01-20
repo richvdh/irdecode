cfb34375d21b44d61677bd4fcf566384c063cd7f
Richard van der Hoff <richard@matrix.org>
2017-01-20 08:51:25 +0000

    mode2 decoder

---
+++ irdecode.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+#
+# Decode the pulse/space output of `mode2` into bits and bytes
+#
+# Usage:
+#    mode2 -d /dev/lirc0 | irdecode.py
+
+
+# timings are currently somewhat hardcoded here.
+
+from __future__ import print_function
+
+import logging
+import select
+import sys
+
+def read(f):
+    pulse_width = None
+
+    logger = logging.getLogger("reader")
+
+    pulseDecoder = PulseDecoder()
+
+    while True:
+        r, w, e = select.select([f], [], [f], 0.1)
+
+        if f in e:
+            return
+
+        if f not in r:
+            # timeout
+            if pulse_width:
+                pulseDecoder.pulse(pulse_width, None)
+                pulse_width = None
+            continue
+
+        line = f.readline().strip()
+        if line is None:
+            return
+
+        try:
+            (t, d) = line.split(" ", 1)
+            d = int(d)
+        except:
+            logger.warn("Cannot parse %s", line)
+            continue
+
+        if t == "pulse":
+            pulse_width = d
+            space_width = 0
+            continue
+
+        if t != "space":
+            logger.error("Cannot parse %s (%s)", line, t)
+            continue
+
+        space_width = d
+
+        if pulse_width is None:
+            logger.info("gap %s us", space_width)
+            continue
+
+        pulseDecoder.pulse(pulse_width, space_width)
+        pulse_width = None
+
+
+class BitDecoder:
+    def __init__(self):
+        self._reset()
+        self.logger = logging.getLogger("bitdecoder")
+        self.byte_buffer = ""
+
+    def _reset(self):
+        self.bit_count = 0
+        self.bit_buffer = 0
+
+    def bit(self, val):
+        self.bit_buffer = (self.bit_buffer << 1) | (val & 0x1)
+        self.bit_count += 1
+        if self.bit_count >= 8:
+            self.logger.debug("Byte: %02x", self.bit_buffer)
+            self.byte_buffer += "%02x" % self.bit_buffer
+            self._reset()
+
+    def gap(self):
+        if self.bit_count != 0:
+            self.logger.warn("Discarding %i bits", self.bit_count)
+        self.logger.info("Read: %s" % self.byte_buffer)
+        self._reset()
+        self.byte_buffer=""
+
+
+class PulseDecoder:
+    def __init__(self):
+        self.logger = logging.getLogger("PulseDecoder")
+        self.bitDecoder = BitDecoder()
+
+    def pulse(self, pulse_width, space_width):
+        if (pulse_width > 550 and pulse_width < 750 and
+            (space_width is None or space_width > 10000)):
+            self.logger.info("gap %s us", space_width)
+            self.bitDecoder.gap()
+            return
+
+        if (pulse_width > 4400 and pulse_width < 4600 and
+            space_width > 4400 and space_width < 4600):
+            self.logger.debug("header")
+            return
+
+        if (pulse_width > 520 and pulse_width < 750 and
+            space_width > 1500 and space_width < 1700):
+            self.logger.debug("one")
+            self.bitDecoder.bit(1)
+            return
+
+        if (pulse_width > 520 and pulse_width < 750 and
+            space_width > 350 and space_width < 570):
+            self.logger.debug("zero")
+            self.bitDecoder.bit(0)
+            return
+
+
+        self.logger.warn("Unknown %d %d", pulse_width, space_width)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    #logging.getLogger("reader").setLevel(logging.WARN)
+    read(sys.stdin)
