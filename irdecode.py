73929319e70ad554912d6d98f7ceab37782ee7d7
Richard van der Hoff <richard@matrix.org>
2017-02-12 23:50:19 +0000

    tweak timings

--- irdecode.py
+++ irdecode.py
@@ -14,6 +14,26 @@ import logging
 import select
 import sys
 
+# the min and max dirations of the pulse and space parts of the header
+MIN_HEADER_PULSE_WIDTH=4400
+MAX_HEADER_PULSE_WIDTH=4800
+MIN_HEADER_SPACE_WIDTH=MIN_HEADER_PULSE_WIDTH
+MAX_HEADER_SPACE_WIDTH=MAX_HEADER_PULSE_WIDTH
+
+# the min and max durations of the pulse part of a one, zero, or trailer
+MIN_PULSE_WIDTH=400
+MAX_PULSE_WIDTH=750
+
+# the min and max durations of the space part of a zero
+MIN_ZERO_SPACE_WIDTH=350
+MAX_ZERO_SPACE_WIDTH=750
+
+# the min and max durations of the space part of a one
+MIN_ONE_SPACE_WIDTH=1500
+MAX_ONE_SPACE_WIDTH=1820
+
+
+
 def read(f):
     pulse_width = None
 
@@ -64,6 +84,46 @@ def read(f):
         pulse_width = None
 
 
+class PulseDecoder:
+    def __init__(self):
+        self.logger = logging.getLogger("PulseDecoder")
+        self.bitDecoder = BitDecoder()
+
+    def pulse(self, pulse_width, space_width):
+        if (pulse_width > MIN_PULSE_WIDTH and
+            pulse_width < MAX_PULSE_WIDTH and
+            (space_width is None or space_width > 10000)):
+            self.logger.info("gap %s us", space_width)
+            self.bitDecoder.gap()
+            return
+
+        if (pulse_width > MIN_HEADER_PULSE_WIDTH and
+            pulse_width < MAX_HEADER_PULSE_WIDTH and
+            space_width > MIN_HEADER_SPACE_WIDTH and
+            space_width < MAX_HEADER_SPACE_WIDTH):
+            self.logger.debug("header")
+            return
+
+        if (pulse_width > MIN_PULSE_WIDTH and
+            pulse_width < MAX_PULSE_WIDTH and
+            space_width > MIN_ONE_SPACE_WIDTH and
+            space_width < MAX_ONE_SPACE_WIDTH):
+            self.logger.debug("one")
+            self.bitDecoder.bit(1)
+            return
+
+        if (pulse_width > MIN_PULSE_WIDTH and
+            pulse_width < MAX_PULSE_WIDTH and
+            space_width > MIN_ZERO_SPACE_WIDTH and
+            space_width < MAX_ZERO_SPACE_WIDTH):
+            self.logger.debug("zero")
+            self.bitDecoder.bit(0)
+            return
+
+
+        self.logger.warn("Unknown %d %d", pulse_width, space_width)
+
+
 class BitDecoder:
     def __init__(self):
         self._reset()
@@ -90,40 +150,8 @@ class BitDecoder:
         self.byte_buffer=""
 
 
-class PulseDecoder:
-    def __init__(self):
-        self.logger = logging.getLogger("PulseDecoder")
-        self.bitDecoder = BitDecoder()
-
-    def pulse(self, pulse_width, space_width):
-        if (pulse_width > 550 and pulse_width < 750 and
-            (space_width is None or space_width > 10000)):
-            self.logger.info("gap %s us", space_width)
-            self.bitDecoder.gap()
-            return
-
-        if (pulse_width > 4400 and pulse_width < 4600 and
-            space_width > 4400 and space_width < 4600):
-            self.logger.debug("header")
-            return
-
-        if (pulse_width > 520 and pulse_width < 750 and
-            space_width > 1500 and space_width < 1700):
-            self.logger.debug("one")
-            self.bitDecoder.bit(1)
-            return
-
-        if (pulse_width > 520 and pulse_width < 750 and
-            space_width > 350 and space_width < 570):
-            self.logger.debug("zero")
-            self.bitDecoder.bit(0)
-            return
-
-
-        self.logger.warn("Unknown %d %d", pulse_width, space_width)
-
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     #logging.getLogger("reader").setLevel(logging.WARN)
+    #logging.getLogger("PulseDecoder").setLevel(logging.DEBUG)
     read(sys.stdin)
